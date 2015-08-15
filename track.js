da418a871fd6b0a0b4de30772b33875580ca6e70
elgehelge <helgemunkjacobsen@gmail.com>
2015-08-15 02:16:22 +0200

    better uuid generator

--- track.js
+++ track.js
@@ -1,10 +1,12 @@
 // Generate a unique ID to refer to this specefic page load
 function uuidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4();
-}
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+        var r, v;
+        r = Math.random() * 16 | 0;
+        v = char === 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
+    });
+};
 _basset.uuid = uuidGenerator();
 
 // Helper functions
@@ -12,14 +14,14 @@ function sendData() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", "www.api.basset.io/endpoint", true);
     xmlHttp.send(_basset);
-}
+};
 
 
 // Fingerprinting
 function detectFingerprint() {
 	// TODO: Do browser fingerprinting here
 	sendData();
-}
+};
 
 
 // Content capturing
@@ -29,7 +31,7 @@ function capturePageContent() {
 	_basset.page.url = window.location.href;
 	_basset.page.title = document.title
 	sendData();
-}
+};
 
 // Form listeners
 function addFormListeners() {
@@ -41,7 +43,7 @@ function addFormListeners() {
 
 	// var submitButton;
 	// add sendData() to submitbutton (put it in from of other function calls hooked up to the button)
-}
+};
 
 
 // Call what should be called immediatly, and hook it rest onto the window.onload event
@@ -55,4 +57,4 @@ if (typeof window.onload != 'function') {
         capturePageContent();
         addFormListeners();
     }
-}
+};
